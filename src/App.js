a22ff5e19d704dcea54c4e038970c28e19de8c88
Aaron Wilder <element1028@gmaill.com>
2018-07-07 14:17:26 -0700

    Blank CRA to learn more about lifting state up in React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Temperature Calculator for the boiling point of water.</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
