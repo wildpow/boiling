b3a4a798e041317bb60eb5f91ab074ee464ed04e
Aaron Wilder <element1028@gmaill.com>
2018-07-07 14:33:26 -0700

    basic input conditionally renders text response if water would boil at the number given

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
+import Calculator from './Calculator';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Temperature Calculator for the boiling point of water.</h1>
-        
+        <h1>
+          Temperature Calculator
+          for the boiling point of water.
+        </h1>
+        <Calculator/>
       </div>
     );
   }
